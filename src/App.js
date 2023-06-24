47f5aa2043db09a44c6cd83557f58fc32c94bc6c
Suraj JAwrani <surajjawrani2022@gmail.com>
2023-06-24 14:04:11 +0530

    converted template into a react website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import NavBar from "./Navbar";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import AppSection from "./AppSection";
+import CardSection from "./CardSection";
 
 
 const App = () => {
     return (
         <div>
             <NavBar />
+            <HeroSection />
+            <AppSection />
+            <CardSection />
+            <Footer />
         </div>
     )
 };
